c635b566a583a80167c46e77545611af449aa7e3
Ayaz <baickov@gmail.com>
2019-11-19 20:53:42 +0300

    Changed table attributes references

--- src/init.sql
+++ src/init.sql
@@ -1,75 +1,81 @@
+CREATE SCHEMA hospital;
+
 CREATE TABLE Employee(
-    id serial PRIMARY KEY,
+    id integer PRIMARY KEY,
     name varchar(30),
     salary money
 );
 
 CREATE TABLE Client(
-    id serial PRIMARY KEY,
+    id integer PRIMARY KEY,
     name varchar(30),
     ssn integer UNIQUE,
     credit_info varchar(10)
 );
 
 CREATE TABLE Doctor(
-    employee_id integer references Employee,
+    employee_id integer references Employee(id),
     speciality varchar(30),
     cabinet integer,
     PRIMARY KEY (employee_id)
 );
 
 CREATE TABLE DoctorsAssistant(
-    empployee_id integer references Employee,
-    assists integer references Doctor,
+    empployee_id integer references Employee(id),
+    assists integer references Doctor(employee_id),
     PRIMARY KEY (empployee_id)
 );
 
 CREATE TABLE Appointment(
-    doctor_id integer references Doctor,
-    client_id integer references Client,
-    datetime timestamptz,
+    doctor_id integer references Doctor(employee_id),
+    client_id integer references Client(id),
+    date date,
+    time time,
     approved boolean,
-    PRIMARY KEY (doctor_id, client_id, datetime)
+    PRIMARY KEY (doctor_id, client_id, date, time)
 );
 
 CREATE TABLE Inventory(
-    id serial PRIMARY KEY,
+    id integer PRIMARY KEY,
     usage_receipt varchar(30)
 );
 
 CREATE TABLE InventoryRequests(
-    inventory_id integer references Inventory,
-    assistant_id integer references DoctorsAssistant,
-    date timestamptz,
+    inventory_id integer references Inventory(id),
+    assistant_id integer references DoctorsAssistant(empployee_id),
+    date date,
+    time time,
     approved boolean,
-    PRIMARY KEY(inventory_id, date)
+    PRIMARY KEY(inventory_id, date, time)
 );
 CREATE TABLE Procedures(
-    id serial PRIMARY KEY,
-    assistant_id integer references DoctorsAssistant,
-    client_id integer references Client,
-    inventory_id integer references Inventory,
+    id integer PRIMARY KEY,
+    assistant_id integer references DoctorsAssistant(empployee_id),
+    client_id integer references Client(id),
+    inventory_id integer references Inventory(id),
     result text,
     cost money,
-    date timestamptz
+    date date,
+    time time
 );
 CREATE TABLE PatientRecord(
-    id serial PRIMARY KEY,
+    id integer PRIMARY KEY,
     personal_info text,
-    client_id integer references Client
+    client_id integer references Client(id)
 );
 
 CREATE TABLE TreatmentReport(
-    id serial PRIMARY KEY,
-    doctor_id integer references Doctor,
-    record_id integer references PatientRecord,
-    date timestamptz,
+    id integer PRIMARY KEY,
+    doctor_id integer references Doctor(employee_id),
+    record_id integer references PatientRecord(id),
+    date date,
+    time time,
     description text
 );
 
 CREATE TABLE TreatmentTests(
-    report_id integer references TreatmentReport,
-    procedure_id integer references Procedures,
+    report_id integer references TreatmentReport(id),
+    procedure_id integer references Procedures(id),
     PRIMARY KEY (report_id, procedure_id)
 );
 
@@ -80,25 +86,28 @@ CREATE TABLE Drug(
 );
 
 CREATE TABLE Prescribed(
-    record_id integer references PatientRecord,
-    drug varchar(30) references Drug,
+    record_id integer references PatientRecord(id),
+    drug varchar(30) references Drug(name),
     prescribed_amount integer,
     PRIMARY KEY(record_id, drug)
 );
 
 CREATE TABLE OnlineHelp(
-    employee_id integer references Employee,
-    client_id integer references Client,
-    date timestamptz,
+    employee_id integer references Employee(id),
+    client_id integer references Client(id),
+    date date,
+    time time,
     question text,
     answer text,
     PRIMARY KEY (client_id, date)
 );
 
 CREATE TABLE DrugSold(
-    drug varchar(30) references Drug,
-    client_id integer references Client,
+    drug varchar(30) references Drug(name),
+    client_id integer references Client(id),
     amount integer,
-    datetime timestamptz,
-    PRIMARY KEY (client_id, datetime)
+    date date,
+    time time,
+    PRIMARY KEY (client_id, date, time)
 );
+
