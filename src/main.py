a891b5dbba1729f2f0aacd8f027d63b130e23f34
i1i1 <vanyarybin1@live.ru>
2019-11-25 21:39:59 +0300

    Nice and Fancy Gui

--- src/main.py
+++ src/main.py
@@ -116,7 +116,8 @@ def url_home():
     for q in queries.keys():
         buttons[q] = "/query/" + q
 
-    return render_template("home.html", buttons=buttons, tmp=buttons)
+    return render_template("home.html", buttons=buttons,
+                           default_css=default_css)
 
 
 @app.route('/populate', methods=['POST', 'GET'])
@@ -133,6 +134,7 @@ def url_populate():
     if request.method == 'GET':
         return render_template("query.html", error="", url='/populate',
                                name="Populate",
+                               default_css=default_css,
                                args=args.keys())
 
     query = ""
